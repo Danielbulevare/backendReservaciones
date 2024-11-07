343f544eaa9cfe63c3cf6d2811ca1ca746613419
Daniel Rod <jkdaniel06@gmail.com>
2024-11-07 13:09:29 -0600

    Primer commit

---
+++ backendReservaciones/src/main/java/com/dan/backendReservaciones/BackendReservacionesApplication.java
@@ -0,0 +1,13 @@
+package com.dan.backendReservaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReservacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendReservacionesApplication.class, args);
+	}
+
+}
