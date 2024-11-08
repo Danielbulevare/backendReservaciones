3dfad5d059e1c0110e66427be8bf2b600e3f97b6
Daniel Rod <jkdaniel06@gmail.com>
2024-11-08 09:13:36 -0600

    Se estableció la relación entre el User y Role

--- backendReservaciones/src/main/java/com/dan/backendReservaciones/entity/User.java
+++ backendReservaciones/src/main/java/com/dan/backendReservaciones/entity/User.java
@@ -4,9 +4,13 @@ import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -42,4 +46,8 @@ public class User {
 	@Column(name = "password", nullable = false)
 	@Length(min = 8, max = 20)
 	private String password;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "fk_id_role", referencedColumnName = "id_role", nullable = false)
+	private Role userRole;
 }
