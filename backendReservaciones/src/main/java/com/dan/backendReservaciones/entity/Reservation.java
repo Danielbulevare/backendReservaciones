3a9c2fa93f1c89dc2f65ba80f358e66f9c79849d
Daniel Rod <jkdaniel06@gmail.com>
2024-11-12 22:26:28 -0600

    Se estableció la relación entre la Reservation y el User

--- backendReservaciones/src/main/java/com/dan/backendReservaciones/entity/Reservation.java
+++ backendReservaciones/src/main/java/com/dan/backendReservaciones/entity/Reservation.java
@@ -8,9 +8,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,4 +38,8 @@ public class Reservation {
 	@Enumerated(EnumType.STRING)
 	@Column(name = "status", nullable = false, columnDefinition = "ENUM('RESERVADO', 'CANCELADO') DEFAULT 'RESERVADO'")
 	private Status status;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "fk_id_user", referencedColumnName = "user_id", nullable = false)
+	private User user;
 }
