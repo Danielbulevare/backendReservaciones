adb8a550e95547a0b02c66c8f45f4103e529f7af
Daniel Rod <jkdaniel06@gmail.com>
2024-11-22 12:25:25 -0600

    Se agregó la anotación @CrossOrigin a los controladores

--- backendReservaciones/src/main/java/com/dan/backendReservaciones/controller/UserController.java
+++ backendReservaciones/src/main/java/com/dan/backendReservaciones/controller/UserController.java
@@ -2,6 +2,7 @@ package com.dan.backendReservaciones.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	@Autowired
 	UserService userService;
